8fc587162b26a93cebb03c5fedd437cf68a9b45e
Jakob <jakob_bjorner@hotmail.com>
2020-10-17 23:46:29 -0400

    made object oriented

--- adjective_polarization.py
+++ adjective_polarization.py
@@ -4,9 +4,11 @@ from newspaper import fulltext
 import glob
 from textblob import TextBlob
 
-class ArticleScrapper():
-    url = 'https://www.foxnews.com/us/free-speech-rally-marred-by-violence-as-counterprotesters-storm'
 
+class ArticleScrapper():
+    url1 = 'https://www.foxnews.com/us/free-speech-rally-marred-by-violence-as-counterprotesters-storm'
+    def __init__(self, url=url1):
+        self.url = url
     # def check_update():
     #     filename = None
     #     path = "json/query_*.json"
@@ -16,15 +18,17 @@ class ArticleScrapper():
     #         filename = sorted(glob.glob(path))[len(glob.glob(path))-1]
     #     return filename
 
-    def parseURL(self, url):
+    def parseURL(self, url = url1):
         article = Article(url)
         metrics = {}
-        
         articleText = fulltext(requests.get(url).text)
         articleSentiment = TextBlob(articleText).sentiment
         metrics["polarity"] = articleSentiment.polarity
+
         metrics["subjectivity"] = articleSentiment.subjectivity
         print(metrics)
         return metrics
-
-    parseURL(url)
+if __name__ == "__main__":
+    a = ArticleScrapper()
+    a.parseURL()
+    
