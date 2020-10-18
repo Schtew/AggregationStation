a4494d82f34cac0c892b82389ea54ddc32187fda
jakob-bjorner <47092630+jakob-bjorner@users.noreply.github.com>
2020-10-17 23:19:20 -0400

    subjectivity and polarity of an article (commit to "main")

--- adjective_polarization.py
+++ adjective_polarization.py
@@ -1,27 +1,29 @@
+import requests
 from newspaper import Article
+from newspaper import fulltext
 import glob
+from textblob import TextBlob
 
 class ArticleScrapper():
     url = 'https://www.usatoday.com/story/news/politics/2020/10/17/fbi-probes-possible-russia-link-hunter-biden-data-trump-ally-giuliani/3661895001/'
 
-    def check_update():
-        filename = None
-        path = "json/query_*.json"
-        if glob.glob(path) == []:
-            print("Existing data file not found...")
-        else:
-            filename = sorted(glob.glob(path))[len(glob.glob(path))-1]
-        return filename
+    # def check_update():
+    #     filename = None
+    #     path = "json/query_*.json"
+    #     if glob.glob(path) == []:
+    #         print("Existing data file not found...")
+    #     else:
+    #         filename = sorted(glob.glob(path))[len(glob.glob(path))-1]
+    #     return filename
 
     def parseURL(url):
         article = Article(url)
-
-        print(article.text)
-        if article.authors != None:
-            authors = article.authors
-
-
-        metrics = {"author": authors}
+        metrics = {}
+        
+        articleText = fulltext(requests.get(url).text)
+        articleSentiment = TextBlob(articleText).sentiment
+        metrics["polarity"] = articleSentiment.polarity
+        metrics["subjectivity"] = articleSentiment.subjectivity
         print(metrics)
 
     parseURL(url)
