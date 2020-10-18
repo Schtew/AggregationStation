b293ebf3432b991455ce3001923bdcae42140929
Jakob <jakob_bjorner@hotmail.com>
2020-10-17 23:59:32 -0400

    object oriented testing between adjective polarity and polBiasScraper

--- PolBiasScraper.py
+++ PolBiasScraper.py
@@ -1,16 +1,21 @@
 import requests 
 from bs4 import BeautifulSoup
+from adjective_polarization import ArticleScrapper
 
 class PolBiasScraper():
-    URL = 'https://mediabiasfactcheck.com/ABC-australia'
+    URL = 'https://mediabiasfactcheck.com/the-australian'
     # results = soup.select('.entry-title')[0]
-
-    def parseURL(URL):
-        page = requests.get(URL)
+    def __init__(self, url = URL):
+        self.url = url
+    def parseURL(self, url = URL):
+        page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
         results = soup.find("img",{"data-attachment-id": True})["alt"]
     #    print(results.split(" - ")[3])
         for x in results.split(" - "):
             print(x)
-
-    parseURL(URL)
+if __name__ == "__main__":
+    p = PolBiasScraper()
+    p.parseURL()
+    a = ArticleScrapper()
+    a.parseURL()
